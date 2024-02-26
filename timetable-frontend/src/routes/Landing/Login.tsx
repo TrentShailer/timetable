bfedbdfa58cb9e316d8cb5934e9c494ff3a7d59b
Trent Shailer <trent.shailer@gmail.com>
2024-02-26 16:44:19 +1300

    fix redirect to home when reloading on /account

--- timetable-frontend/src/routes/Landing/Login.tsx
+++ timetable-frontend/src/routes/Landing/Login.tsx
@@ -10,58 +10,58 @@ import { useState } from "react";
 import { get_error_description } from "../../utils/errors";
 
 export default function Login() {
-  const toast = useToast();
-  const [, setLocation] = useLocation();
-  const [, setUser] = useAtom(userAtom);
-  const [loading, setLoading] = useState(false);
+	const toast = useToast();
+	const [, setLocation] = useLocation();
+	const [, setUser] = useAtom(userAtom);
+	const [loading, setLoading] = useState(false);
 
-  const onSuccess = async (code: string) => {
-    try {
-      const response = await axios.post<User>("/auth/google", {
-        code,
-      });
+	const onSuccess = async (code: string) => {
+		try {
+			const response = await axios.post<User>("/auth/google", {
+				code,
+			});
 
-      setUser(response.data);
-      setLocation("/home");
-    } catch (error) {
-      console.error(error);
-      const toast_description = get_error_description(error);
-      toast({
-        title: "Failed to authenticate with server",
-        description: toast_description,
-        status: "error",
-        duration: 5000,
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
+			setUser(response.data);
+			setLocation("/home");
+		} catch (error) {
+			console.error(error);
+			const toast_description = get_error_description(error);
+			toast({
+				title: "Failed to authenticate with server",
+				description: toast_description,
+				status: "error",
+				duration: 5000,
+			});
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const login = useGoogleLogin({
-    onSuccess: async ({ code }) => {
-      onSuccess(code);
-    },
-    onError: async () => {
-      setLoading(false);
-    },
-    onNonOAuthError: async () => {
-      setLoading(false);
-    },
-    flow: "auth-code",
-  });
+	const login = useGoogleLogin({
+		onSuccess: async ({ code }) => {
+			onSuccess(code);
+		},
+		onError: async () => {
+			setLoading(false);
+		},
+		onNonOAuthError: async () => {
+			setLoading(false);
+		},
+		flow: "auth-code",
+	});
 
-  return (
-    <Button
-      isLoading={loading}
-      w="100%"
-      rightIcon={<Icon boxSize="5" as={HiOutlineArrowRight} />}
-      colorScheme="purple"
-      onClick={() => {
-        setLoading(true);
-        login();
-      }}
-    >
-      Get Started
-    </Button>
-  );
+	return (
+		<Button
+			isLoading={loading}
+			w="100%"
+			rightIcon={<Icon boxSize="5" as={HiOutlineArrowRight} />}
+			colorScheme="purple"
+			onClick={() => {
+				setLoading(true);
+				login();
+			}}
+		>
+			Get Started
+		</Button>
+	);
 }
