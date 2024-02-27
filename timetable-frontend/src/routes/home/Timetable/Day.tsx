727d44a1364af3b2f1a7043a35d8ac93e2e95124
Trent Shailer <trent.shailer@gmail.com>
2024-02-27 13:25:09 +1300

    improved day display based on blocks

--- timetable-frontend/src/routes/home/Timetable/Day.tsx
+++ timetable-frontend/src/routes/home/Timetable/Day.tsx
@@ -1,8 +1,9 @@
 import { Block, Course } from "../../../utils/types";
-import { Card, Flex, TabPanel } from "@chakra-ui/react";
+import { Card, TabPanel } from "@chakra-ui/react";
 import BlockDisplay from "./Day/BlockDisplay";
 import TimetableGrid from "./Day/TimetableGrid";
 import CurrentTime from "./Day/CurrentTime";
+import { useEffect, useState } from "react";
 
 type Props = {
     week_day: number;
@@ -19,6 +20,21 @@ export default function Day({
     isCurrentDay,
     blockColumns,
 }: Props) {
+    const [maxColumn, setMaxColumn] = useState(0);
+
+    useEffect(() => {
+        let max_column = 0;
+
+        blocks.forEach((block) => {
+            let column = blockColumns[block.id];
+            if (column > maxColumn) {
+                max_column = column;
+            }
+        });
+
+        setMaxColumn(max_column);
+    }, [blocks, blockColumns]);
+
     return (
         <TabPanel
             p={0}
@@ -28,37 +44,41 @@ export default function Day({
             maxW="100vw"
             tabIndex={week_day}
         >
-            <Flex h="100%" justify="center">
-                <Card overflowX="auto" minW={300} w="100%" h="100%">
-                    <TimetableGrid />
+            <Card
+                ml={`max(calc(50% - ${
+                    (60 + 155 * (maxColumn + 1)) / 2
+                }px), 0px)`}
+                h="100%"
+                w={60 + 155 * (maxColumn + 1)}
+            >
+                <TimetableGrid />
 
-                    {blocks.map((block) => {
-                        const courseMatches = courses.filter(
-                            (course) => course.id == block.course_id
-                        );
-                        if (courseMatches.length === 0) {
-                            console.error("Failed to find a matching course");
-                            return;
-                        }
+                {blocks.map((block) => {
+                    const courseMatches = courses.filter(
+                        (course) => course.id == block.course_id
+                    );
+                    if (courseMatches.length === 0) {
+                        console.error("Failed to find a matching course");
+                        return;
+                    }
 
-                        let column = blockColumns[block.id];
-                        if (column === undefined) {
-                            console.error(`Block: ${block} has no column`);
-                            return;
-                        }
+                    let column = blockColumns[block.id];
+                    if (column === undefined) {
+                        console.error(`Block: ${block} has no column`);
+                        return;
+                    }
 
-                        return (
-                            <BlockDisplay
-                                key={block.id}
-                                block={block}
-                                course={courseMatches[0]}
-                                column={column}
-                            />
-                        );
-                    })}
-                    {isCurrentDay ? <CurrentTime /> : null}
-                </Card>
-            </Flex>
+                    return (
+                        <BlockDisplay
+                            key={block.id}
+                            block={block}
+                            course={courseMatches[0]}
+                            column={column}
+                        />
+                    );
+                })}
+                {isCurrentDay ? <CurrentTime /> : null}
+            </Card>
         </TabPanel>
     );
 }
--- timetable-frontend/src/routes/home/Timetable/Day/TimetableGrid.tsx
+++ timetable-frontend/src/routes/home/Timetable/Day/TimetableGrid.tsx
@@ -5,22 +5,23 @@ export const startHour = 7;
 export const nHours = 12;
 
 export default function TimetableGrid() {
-  return (
-    <Box h="100%">
-      {Array.from(Array(nHours).keys()).map((_, i) => (
-        <Box
-          key={i}
-          h={`${100 / nHours}%`}
-          borderColor="gray.600"
-          borderWidth="1px"
-          borderLeft="none"
-          borderRight="none"
-        >
-          <Text pl={1} fontSize="sm" color="gray.600">
-            {dayjs(`${startHour + i}`, "HH").format("h:mma")}
-          </Text>
+    return (
+        <Box h="100%" w={"100%"}>
+            {Array.from(Array(nHours).keys()).map((_, i) => (
+                <Box
+                    key={i}
+                    h={`${100 / nHours}%`}
+                    w={"100%"}
+                    borderColor="gray.600"
+                    borderWidth="1px"
+                    borderLeft="none"
+                    borderRight="none"
+                >
+                    <Text pl={1} fontSize="sm" color="gray.600">
+                        {dayjs(`${startHour + i}`, "HH").format("h:mma")}
+                    </Text>
+                </Box>
+            ))}
         </Box>
-      ))}
-    </Box>
-  );
+    );
 }
